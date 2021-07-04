23c6f1fe54430f39ea82b4daed179f09bac8cbe2
Margo <margoshka.ri6@gmail.com>
2021-07-04 14:36:46 +0300

    Add OpenFileDialog

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,22 @@ namespace Gallery
 {
     public partial class Form1 : Form
     {
+        private string directoryPath = string.Empty;
         public Form1()
         {
             InitializeComponent();
+            OpenFile();
+        }
+        private void OpenFile()
+        {
+            using(OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Images|*.png; *.jpg";
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    directoryPath = Path.GetDirectoryName(dialog.FileName);
+                }
+            }
         }
     }
 }
