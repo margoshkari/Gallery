e2167cc79d43c5346eed675006d61d0853195af4
Margo <margoshka.ri6@gmail.com>
2021-07-04 14:55:11 +0300

    Add GUI to Form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,62 @@ namespace Gallery
         /// </summary>
         private void InitializeComponent()
         {
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.prevBtn.Location = new System.Drawing.Point(12, 190);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(50, 44);
+            this.prevBtn.TabIndex = 0;
+            this.prevBtn.Text = "◀";
+            this.prevBtn.UseVisualStyleBackColor = true;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.nextBtn.Location = new System.Drawing.Point(738, 190);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(50, 44);
+            this.nextBtn.TabIndex = 1;
+            this.nextBtn.Text = "▶";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox.Location = new System.Drawing.Point(58, 6);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(680, 349);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.prevBtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
